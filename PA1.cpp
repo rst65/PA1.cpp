d9a57e4747c58b1700025f1a6a1e332bd4e8613e
rst65 <157853263+rst65@users.noreply.github.com>
2024-02-29 23:03:22 -0600

    Update PA1.cpp

--- PA1.cpp
+++ PA1.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 
 using namespace std;
 
-double rand_gen() {
-    return static_cast<double>(rand()) / (RAND_MAX + 1.0);
-}
-
 double exp_rand(double lambda) {
-    return (1.0 - rand_gen()) / lambda;
-}
+    double u = static_cast<double>(rand()) / RAND_MAX;
+    return -log(1.0 - u) / lambda;}
 
 int main() {
     const int num_processes = 1000;
     const double arrivalRate = 2.0;
     const double serviceTime = 1.0;
-    srand(static_cast<unsigned>(time(nullptr)));
+
+    cout << "Process ID\tArrival Time\tService Time" << endl;
+
     double currentTime = 0.0;
 
     for (int i = 1; i <= num_processes; ++i) {
-double arrivalTime = exp_rand(arrivalRate);
-double requestedServiceTime = exp_rand(1.0 / serviceTime);
-        // Print process information
-        cout << "< Process ID: " << i << " Arrival time: " << currentTime << " Requested service time: " << requestedServiceTime << ">" << endl;
+
+        double arrivalTime = exp_rand(arrivalRate);
+        currentTime += arrivalTime;
+        double requestedserviceTime = exp_rand(1.0 / serviceTime);
+
+        cout << i << "  \t\t" << currentTime << "  \t\t" << requestedserviceTime << endl;
     }
+
+    double actual_Rate = num_processes / currentTime;
+    double actual_Time = serviceTime;
+
+    cout << "\n<Actual Average Arrival Rate: " << actual_Rate << " processes per second" << ", Actual Average Service Time: " << actual_Time << " seconds>" << std::endl;
+
+    return 0;
+}
